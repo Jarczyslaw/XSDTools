89e91a0fd529d0f107b00976e879619162bbed59
Jarosław Tyma <tyma.jaroslaw@gmail.com>
2020-06-02 21:25:39 +0200

    dev

---
+++ Apps/Desktop/XSDTools.DesktopApp/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using XSDTools.DesktopApp.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+using JToolbox.Desktop.Dialogs;
+
+namespace XSDTools.DesktopApp
+{
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IDialogsService, DialogsService>();
+        }
+    }
+}
---
+++ Apps/Desktop/XSDTools.DesktopApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using JToolbox.Desktop.Dialogs;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace XSDTools.DesktopApp.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string logs = string.Empty;
+        private readonly IDialogsService dialogsService;
+
+        public MainWindowViewModel(IDialogsService dialogsService)
+        {
+            this.dialogsService = dialogsService;
+        }
+
+        public DelegateCommand RemoveExternalDependenciesCommand => new DelegateCommand(() =>
+        {
+        });
+
+        public DelegateCommand GenerateModelsCommand => new DelegateCommand(() =>
+        {
+        });
+
+        public DelegateCommand GenerateSampleXmlCommand => new DelegateCommand(() =>
+        {
+        });
+
+        public DelegateCommand ValidateXmlCommand => new DelegateCommand(() =>
+        {
+        });
+
+        public string Logs
+        {
+            get => logs;
+            set => SetProperty(ref logs, value);
+        }
+    }
+}
